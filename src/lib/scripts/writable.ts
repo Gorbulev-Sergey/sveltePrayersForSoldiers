9cee4669befbd3cb522bb532297d6d1f20d3b3d6
Gotbulev-Sergey <gorbulevsv@yandex.ru>
2024-08-23 16:34:28 +0300

    1

---
+++ src/lib/scripts/writable.ts
@@ -0,0 +1,10 @@
+import { writable } from "svelte/store";
+
+export let isNavPanelShow = writable(true);
+export let isTitlesShow = writable(true);
+export let bgColor = writable("bg-light");
+export let textColor = writable("text-dark");
+export let fontFamily = writable("Raleway");
+export let fontSize = writable(1.2);
+export let lineHeight = writable(1.25);
+export let articleInterval = writable(0.75);
